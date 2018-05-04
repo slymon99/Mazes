b05f0a9dd89e9480611f308d590aeaa37a11879e
Simon_Clark <simonclark236@gmail.com>
2018-05-04 18:18:44 -0400

    Added constructors and initialize method to set up unconnected grid of cells.

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -8,16 +8,31 @@ import java.util.Stack;
  */
 public class Grid {
 
-
+    private ArrayList<Cell> cells;
+    private int rows, cols;
+    private Random rand;
 
     public Grid(int rows, int cols, Random rand) {
+        this.rows = rows;
+        this.cols = cols;
+        this.rand = rand;
 
+        initialize();
+    }
+
+    private void initialize(){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Cell c = new Cell(j * Cell.DIMENSIONS, i * Cell.DIMENSIONS);
+                cells.add(c);
+            }
+        }
     }
 
     public void buildMaze() {
         HashSet<Cell> visited = new HashSet<Cell>();
         Stack<Cell> worklist = new Stack<Cell>();
 
-
+        worklist.push(cells.get(0));
     }
 }
